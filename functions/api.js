5b6bea17e073b273d363731b3ad8accddb978861
Angela <masterangel5858@gmail.com>
2024-03-15 19:02:16 +0700

    add loading screen

--- functions/api.js
+++ functions/api.js
@@ -10,6 +10,7 @@ router.get('/:userid/:time/accept', async (req, res) => {
   const time = req.params.time;
 
   try {
+    res.sendFile(path.join(__dirname, 'templates', 'loading.html'));
     // Fetch medicine data for the specified user ID
     const medicineData = await getdata(userId);
 
