11e1ac7095c158ce557b7261ecf2d0678d20989e
Noppanun Wongchitsupha <masterangel5858@gmail.com>
2024-04-16 02:20:34 +0700

    get out update stock

--- functions/api.js
+++ functions/api.js
@@ -180,7 +180,7 @@ router.get('/acceptall/:userid/:time/:timestamp', async (req, res) => {
     if (filteredMedicine.length === 0) {
       return res.send(`No medicine found for ${time}`);
     }
-    await updateStockall(userId,time);
+    // await updateStockall(userId,time);
     // Insert each medicine into the database
     const insertedMedicines = [];
     for (const medicine of filteredMedicine) {
@@ -243,7 +243,7 @@ router.get('/accept/:userid/:MedicName/:timestamp', async (req, res) => {
     if (!selectedMedicine) {
       return res.send(`No medicine found with the name: ${medicName}`);
     }
-    await updateStockMed(userId,medicName);
+    // await updateStockMed(userId,medicName);
     const newMedicineData = {
       LineID: userId,
       MedicName: selectedMedicine.MedicName,
